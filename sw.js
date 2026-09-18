7b4638f894dc6387670ce25cf0557f48662dba67
SimoneGuggia <simoneguggia@gmail.com>
2026-09-18 18:26:02 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,1 @@
+const CACHE='tumoretour-v1'; self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/manifest.webmanifest','/icon-192.png','/icon-512.png'])))); self.addEventListener('fetch',e=>{if(new URL(e.request.url).pathname.startsWith('/api/')) return; e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));});
